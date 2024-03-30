60a2a95cdea042f01e74c70604a2b6b72e772f6a
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-03-30 09:18:22 +0530

    Edit p021

--- p021-prime-or-not/p20.c
+++ p021-prime-or-not/p20.c
@@ -1,7 +1,13 @@
 // WAP to check whether the given number is prime or not
 #include <stdio.h>
 void main(){
-    int num=103;
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", num);
+    if(num==1){
+       printf("It's not prime");
+       return; 
+    }
     for(int i=2; i<num; i++){
         if(num%i==0){
             printf("It's not prime");
