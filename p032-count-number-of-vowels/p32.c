efb85191416212cd9fab9fb87ec4dbe56494d6f1
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-04-23 12:48:05 +0530

    Edit p032

--- p032-count-number-of-vowels/p32.c
+++ p032-count-number-of-vowels/p32.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <ctype.h>
 void main(){
-    char string[20] = "abeay", ch;
-    int count = 0;
+    char string[20] = "abeay kumar", ch;
+    int c = 0, v = 0, s = 0;
     for(int i=0; string[i]!='\0'; i++){
         ch = tolower(string[i]);
         switch (ch){
@@ -11,9 +11,15 @@ void main(){
             case 'e' :
             case 'i' :
             case 'o' :
-            case 'u' : count++;
-                       break;
+            case 'u' : v++;
+            break;
+            default : if( ch >= 'a' && ch <= 'z') c++;
+			break;
         }
+        switch(ch){
+			case ' ' : s++;
+			break;
+		}
     }
-    printf("The String has %d vowels in it!..", count);
+    printf("The String has %d vowels, %d consonants & %d white spaces in it!..\n", v, c, s);
 }
