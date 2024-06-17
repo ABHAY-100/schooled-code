15e57efdaf79b8883a78626b9e370dc835581f69
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-06-17 11:26:13 +0530

    Modified

--- p020-leap-year-or-not/p20.c
+++ p020-leap-year-or-not/p20.c
@@ -5,7 +5,7 @@ void main(){
     if(year%4==0 && year%100!=0){
         printf("It's a leap year");
     }
-    else if(year%100==0 && year%400==0){
+    else if(year%400==0){
         printf("It's a leap year");
     }
     else{
