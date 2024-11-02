2e8e79b7595bdc8db80d53108f139cdc21d2167c
ABHAY <abhaybalakrishnan884@gmail.com>
2024-11-02 18:26:37 +0530

    add p28

--- S03/OOPs Using Java/p028-jdbc-connectivity/CompanyDB.java
+++ S03/OOPs Using Java/p028-jdbc-connectivity/CompanyDB.java
@@ -3,22 +3,21 @@
 import java.sql.*;
 
 public class CompanyDB {
-
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/companydb";
         String username = "root";
         String password = "ontheway#1200";
 
-        try (Connection connection = DriverManager.getConnection(url, username, password); Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery("select * from employees")) {
-            while (resultSet.next()) {
-                System.out.print("\nID: " + resultSet.getInt(1));
-                System.out.print("; Name: " + resultSet.getString(2));
-                System.out.print("; Age: " + resultSet.getInt(3));
-                System.out.print("; Address: " + resultSet.getString(4));
-                System.out.print("; Salary: " + resultSet.getDouble(5));
-                System.out.println("; \n");
+        try (Connection cnn = DriverManager.getConnection(url, username, password); Statement smt = cnn.createStatement(); ResultSet rs = smt.executeQuery("select * from employees")) {
+            while (rs.next()) {
+                System.out.print("id: " + rs.getInt(1));
+                System.out.print("; name: " + rs.getString(2));
+                System.out.print("; age: " + rs.getInt(3));
+                System.out.print("; address: " + rs.getString(4));
+                System.out.print("; salary: " + rs.getDouble(5));
+                System.out.print("; \n");
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
