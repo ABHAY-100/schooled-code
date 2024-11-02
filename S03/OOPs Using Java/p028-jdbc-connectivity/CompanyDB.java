1fe68cd3afe2e44cc033284f88010fe7d9b69a2b
ABHAY <abhaybalakrishnan884@gmail.com>
2024-11-02 19:44:47 +0530

    update for all operations

--- S03/OOPs Using Java/p028-jdbc-connectivity/CompanyDB.java
+++ S03/OOPs Using Java/p028-jdbc-connectivity/CompanyDB.java
@@ -8,16 +8,64 @@ public class CompanyDB {
         String username = "root";
         String password = "ontheway#1200";
 
-        try (Connection cnn = DriverManager.getConnection(url, username, password); Statement smt = cnn.createStatement(); ResultSet rs = smt.executeQuery("select * from employees")) {
-            while (rs.next()) {
-                System.out.print("id: " + rs.getInt(1));
-                System.out.print("; name: " + rs.getString(2));
-                System.out.print("; age: " + rs.getInt(3));
-                System.out.print("; address: " + rs.getString(4));
-                System.out.print("; salary: " + rs.getDouble(5));
-                System.out.print("; \n");
+        // params for inserting
+        // String newName = "Elvin J";
+        // int newAge = 20;
+        // String newAddress = "From Thrissur";
+        // Double newSalary = 30000.00;
+
+        // params for updating
+        // int updateID = 2; 
+        // String updatedAddress = "Updated Address";
+        // double updatedSalary = 350000.00;
+
+        // params for deleting
+        // int deleteID = 4;
+
+        try (Connection cnn = DriverManager.getConnection(url, username, password)) {
+
+            // block for insert
+            // String insertQuery = "INSERT INTO EMPLOYEES (name, age, address, salary) VALUES (?, ?, ?, ?)";
+
+            // try (PreparedStatement psmt = cnn.prepareStatement(insertQuery)) {
+            //     psmt.setString(1, newName);
+            //     psmt.setInt(2, newAge);
+            //     psmt.setString(3, newAddress);
+            //     psmt.setDouble(4, newSalary);
+            //     psmt.executeUpdate();
+            // }
+
+
+            // block for update
+            // String updateQuery = "UPDATE EMPLOYEES SET address = ?, salary = ? where id = ?";
+
+            // try (PreparedStatement psmtForUpdate = cnn.prepareStatement(updateQuery)) {
+            //     psmtForUpdate.setString(1, updatedAddress);
+            //     psmtForUpdate.setDouble(2, updatedSalary);
+            //     psmtForUpdate.setInt(3, updateID);
+            //     psmtForUpdate.executeUpdate();
+            // }
+
+            // block for delete
+            // String deleteQuery = "DELETE FROM EMPLOYEES WHERE ID = ?";
+
+            // try (PreparedStatement psmtForUpdate = cnn.prepareStatement(deleteQuery)) {
+            //     psmtForUpdate.setInt(1, deleteID);
+            //     psmtForUpdate.executeUpdate();
+            // }
+
+            try (Statement smt = cnn.createStatement(); ResultSet rs = smt.executeQuery("select * from employees")) {
+                while (rs.next()) {
+                    System.out.print("id: " + rs.getInt(1));
+                    System.out.print("; name: " + rs.getString(2));
+                    System.out.print("; age: " + rs.getInt(3));
+                    System.out.print("; address: " + rs.getString(4));
+                    System.out.print("; salary: " + rs.getDouble(5));
+                    System.out.print("; \n");
+                }
             }
-        } catch (Exception e) {
+            
+        } catch (SQLException e) {
             System.out.println(e);
         }
     }
