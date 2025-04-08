8c3dae7aa70b213c85667c39a36613e038b7aa9e
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-04-08 13:05:55 +0530

    Rename program (16).R to program_16.R

--- S04/Statistics Methods Lab/program (16).R
+++
@@ -1,4 +0,0 @@
-grades<-c(a=20,b=30,c=25,d=15,f=10)
-colors<-c("red","green","blue","yellow","orange")
-
-pie(grades,labels=paste(names(grades),"(",grades,"%)",sep=""),col=colors,main="percentage of grades in class")
