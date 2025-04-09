f7ec7742e8df0dcd5fe295180bad1cf6c642a82a
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-04-09 13:01:48 +0530

    Update program_23.R

--- S04/Statistics Methods Lab/program_23.R
+++ S04/Statistics Methods Lab/program_23.R
@@ -1,3 +1,5 @@
-data(airquality)
-str(airquality)
-summary(airquality$Temp)
+x = c(15, 18, 21, 15, 21)
+y = c(25, 25, 27, 27, 27)
+
+result = cor(x, y, method = "spearman")
+cat("Spearman correlation coefficient is :", result)
