4d8c25d759944a2b4301e9fe200336b2453cc98a
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-04-09 13:30:06 +0530

    Update program_32.R

--- S04/Statistics Methods Lab/program_32.R
+++ S04/Statistics Methods Lab/program_32.R
@@ -1,18 +1,3 @@
-input <- mtcars[, c("mpg", "disp", "hp", "wt")]
-
-model <- lm(mpg ~ disp + hp + wt, data = input)
-
-print(model)
-
-cat("#### The Coefficient Values ####\n")
-
-a <- coef(model)[1]
-print(a)
-
-Xdisp <- coef(model)[2]
-Xhp <- coef(model)[3]
-Xwt <- coef(model)[4]
-
-print(Xdisp)
-print(Xhp)
-print(Xwt)
+input <- mtcars[,c("am","cyl","hp","wt")]
+am.data = glm(formula = am ~ cyl + hp + wt, data = input, family = binomial)
+print(summary(am.data))
