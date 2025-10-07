2d1ffd65cbe45e916f91cb752413d32d6cc706b6
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-10-07 22:21:28 +0530

    Update client.c

--- S05/Networking/client-server-communication-using-tcp/client.c
+++ S05/Networking/client-server-communication-using-tcp/client.c
@@ -3,49 +3,62 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-#define PORT 80
-#define LOCALHOST inet_addr("127.0.0.1")
+#define PORT 8080
+#define LOCALHOST "127.0.0.1"
 #define BUFFER_SIZE 256
 
-void main()
+struct sockaddr_in create_client_address(const char *server_ip, int port)
 {
-	void error_check(int x, char success[])
-	{
-		if (x < 0)
-		{
-			perror("something went wrong");
-			exit(0);
-		}
-		else
-		{
-			printf("%s\n", success);
-		}
-	}
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(server_ip);
+    
+    return addr;
+}
 
-	int s = socket(AF_INET, SOCK_STREAM, 0);
-	error_check(s, "socket created");
+void error_check(int x, char success[])
+{
+    if (x < 0)
+    {
+        perror("something went wrong");
+        exit(1);
+    }
+    else
+    {
+        printf("%s\n", success);
+    }
+}
 
-	struct sockaddr_in socket_address;
-	socket_address.sin_family = AF_INET;
-	socket_address.sin_addr.s_addr = LOCALHOST;
-	socket_address.sin_port = PORT;
+int main()
+{
+    // 1. Create client socket
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    error_check(s, "socket created");
 
-	int c = connect(s, (const struct sockaddr *)&socket_address, sizeof(socket_address));
-	error_check(c, "connected to server");
+    // 2. Build server address structure
+    struct sockaddr_in server_address = create_client_address(LOCALHOST, PORT);
 
-	char buffer[BUFFER_SIZE] = "";
-	char request[] = "\n ... Hello, this is the Client ... \n";
-	int status = 0;
+    // 3. Connect to the server
+    int c = connect(s, (struct sockaddr *)&server_address, sizeof(server_address));
+    error_check(c, "connected to server");
 
-	status = send(s, request, strlen(request), 0);
-	error_check(status, "message successfully sent to server");
-	status = recv(s, buffer, BUFFER_SIZE, 0);
-	error_check(status, buffer);
+    // 4. Prepare buffers and request message
+    char buffer[BUFFER_SIZE] = "";
+    char request[] = "\n ... Hello, this is the Client ... \n";
 
-	printf(" client-server request-reply process successfully demonstrated using sockets \n .... terminating socket .... \n");
-	close(s);
+    // 5. Send request to server
+    int status = send(s, request, strlen(request), 0);
+    error_check(status, "message successfully sent to server");
+
+    // 6. Receive reply from server
+    status = recv(s, buffer, BUFFER_SIZE, 0);
+    error_check(status, buffer);
+
+    // 7. Close the socket and exit
+    close(s);
+    return 0;
 }
