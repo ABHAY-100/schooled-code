b6615e8097c1e026b6c18fd669ea12a72dd6395e
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-10-07 22:22:09 +0530

    Update client.c

--- S05/Networking/client-server-communication-using-udp/client.c
+++ S05/Networking/client-server-communication-using-udp/client.c
@@ -3,49 +3,59 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
 #define PORT 8080
-#define LOCALHOST inet_addr("127.0.0.1")
+#define LOCALHOST "127.0.0.1"
 #define BUFFER_SIZE 256
 
-void main()
+struct sockaddr_in create_client_address(const char *server_ip, int port)
 {
-    void error_check(int x, char success[])
-    {
-        if (x < 0)
-        {
-            perror("something went wrong");
-            exit(0);
-        }
-        else
-        {
-            printf("%s\n", success);
-        }
-    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(server_ip);
 
+    return addr;
+}
+
+void error_check(int x, char success[])
+{
+    if (x < 0)
+    {
+        perror("something went wrong");
+        exit(1);
+    }
+    else
+    {
+        printf("%s\n", success);
+    }
+}
+
+int main()
+{
+    // 1. Create client socket
     int s = socket(AF_INET, SOCK_DGRAM, 0);
     error_check(s, "socket created");
 
-    struct sockaddr_in socket_address;
-    socket_address.sin_family = AF_INET;
-    socket_address.sin_addr.s_addr = LOCALHOST;
-    socket_address.sin_port = htons(PORT);
+    // 2. Build server address structure
+    struct sockaddr_in server_address = create_client_address(LOCALHOST, PORT);
+    socklen_t server_len = sizeof(server_address);
 
+    // 3. Prepare buffers and request message
     char buffer[BUFFER_SIZE] = "";
     char request[] = "\n ... Hello, this is the Client ... \n";
-    socklen_t len = sizeof(socket_address);
-    int status = 0;
 
-    status = sendto(s, request, strlen(request), 0, (struct sockaddr *)&socket_address, len);
+    // 4. Send request to server
+    int status = sendto(s, request, strlen(request), 0, (struct sockaddr *)&server_address, server_len);
     error_check(status, "message successfully sent to server");
 
-    status = recvfrom(s, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&socket_address, &len);
-    error_check(status, "reply received from server");
-    printf("Received from server: %s", buffer);
+    // 5. Receive reply from server
+    status = recvfrom(s, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&server_address, &server_len);
+    error_check(status, buffer);
 
-    printf(" client-server request-reply process successfully demonstrated using UDP sockets \n .... terminating socket .... \n");
+    // 6. Close the socket and exit
     close(s);
+    return 0;
 }
