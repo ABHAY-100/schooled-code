1b70315c2f0f599bb5b429a96b199716484db44c
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2025-11-02 09:58:26 +0530

    Refactor selective repeat server for new protocol

--- S05/Networking/selective-repeat/server.c
+++ S05/Networking/selective-repeat/server.c
@@ -1,133 +1,63 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <time.h>
+#include <string.h>
+#include <arpa/inet.h>
 
-#define PORT 8080
-#define BUFFER_SIZE 256
+#define PORT 12345
 #define LOCALHOST "127.0.0.1"
 
-#define NUM_PACKETS 5
+typedef struct {
+    int fnum;
+    char data[30];
+} Frame;
 
-#define WINDOW_SIZE 3
+typedef struct {
+    int anum;
+} Ack;
 
-typedef struct
-{
-    int seq_num;
-    int data;
-} Packet;
+int main() {
+    int sock;
+    struct sockaddr_in serveraddr, clientaddr;
+    socklen_t addr_len = sizeof(clientaddr);
+    Frame f;
+    Ack a;
+    int expected[10] = {0}; // buffer for received frames
+    int drop3 = 0;
 
-typedef struct
-{
-    int ack_num;
-} ACK;
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT);
+    serveraddr.sin_addr.s_addr = inet_addr(LOCALHOST);
 
-void error_check(int x, char success[])
-{
-    if (x < 0)
-    {
-        perror("something went wrong!");
-        exit(1);
-    }
-    else
-    {
-        printf("%s\n", success);
-    }
-}
+    bind(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
 
-struct sockaddr_in create_socket_address()
-{
-    struct sockaddr_in socket_address;
-    socket_address.sin_family = AF_INET;
-    socket_address.sin_addr.s_addr = inet_addr(LOCALHOST);
-    socket_address.sin_port = htons(PORT);
+    printf("\n--- SELECTIVE REPEAT ARQ (SERVER) ---\n\n");
 
-    return socket_address;
-}
+    int count = 1;
+    while (count <=10) {
+        int n = recvfrom(sock, &f, sizeof(f), 0, (struct sockaddr *)&clientaddr, &addr_len);
+        if (n <= 0) continue;
 
-int main()
-{
-    // 1. Create server socket
-    int s = socket(AF_INET, SOCK_DGRAM, 0);
-    error_check(s, "socket created!");
+        printf("[S] Frame %d received.\n", f.fnum);
 
-    // 2. Bind the socket to the server address
-    struct sockaddr_in server_address = create_socket_address();
-    int b = bind(s, (struct sockaddr *)&server_address, sizeof(server_address));
-    error_check(b, "binding successful!");
-
-    // 3. Communication with client
-    struct sockaddr_in client_address;
-    socklen_t client_len = sizeof(client_address);
-
-    int expected_seq = 0;
-    int packets_received = 0;
-
-    int buffer[WINDOW_SIZE + 1] = {0};
-    Packet packet_buffer[WINDOW_SIZE + 1];
-
-    // Loss simulation counters
-    int drop_counter_3 = 1;
-    int drop_counter_5 = 1;
-
-    while (packets_received < NUM_PACKETS)
-    {
-        Packet packet;
-
-        int status = recvfrom(s, &packet, sizeof(packet), 0, (struct sockaddr *)&client_address, &client_len);
-        if (status > 0)
-        {
-            printf("\nReceived Data %d (seq_num = %d)\n", packet.data, packet.seq_num);
-
-            // Simulate packet loss
-            if (packet.data == 3 && drop_counter_3 > 0)
-            {
-                printf(">>> Simulating packet loss for data 3\n");
-                drop_counter_3--;
-                continue;
-            }
-            else if (packet.data == 5 && drop_counter_5 > 0)
-            {
-                printf(">>> Simulating packet loss for data 5\n");
-                drop_counter_5--;
-                continue;
-            }
-
-            ACK ack;
-            ack.ack_num = packet.seq_num;
-            status = sendto(s, &ack, sizeof(ack), 0, (struct sockaddr *)&client_address, client_len);
-            if (status > 0)
-            {
-                printf("ACK sent for Data %d (ack_num = %d)\n", packet.data, ack.ack_num);
-            }
-
-            if (packet.seq_num >= expected_seq && packet.seq_num < expected_seq + WINDOW_SIZE)
-            {
-                if (!buffer[packet.seq_num])
-                {
-                    buffer[packet.seq_num] = 1;
-                    packet_buffer[packet.seq_num] = packet;
-                    printf("Packet Buffered (data = %d, seq_num = %d)\n", packet.data, packet.seq_num);
-                }
-
-                while (buffer[expected_seq])
-                {
-                    printf("Packet Delivered (data = %d, seq_num = %d)\n",
-                           packet_buffer[expected_seq].data, expected_seq);
-                    packets_received++;
-                    buffer[expected_seq] = 0;
-                    expected_seq = (expected_seq + 1) % (WINDOW_SIZE + 1);
-                }
-            }
+        // Simulate loss of Frame 3
+        if (f.fnum == 4 && drop3 == 0) {
+            printf("[S] Frame %d lost (no ACK sent).\n", f.fnum);
+            drop3 = 1;
+            continue;
         }
+
+        // Accept and ACK the frame
+        expected[f.fnum] = 1;
+        a.anum = f.fnum;
+        sendto(sock, &a, sizeof(a), 0, (struct sockaddr *)&clientaddr, addr_len);
+        printf("[S] ACK %d sent.\n", a.anum);
+        count++;
+        sleep(1);
     }
 
-    // 4. Close socket
-    close(s);
-
+    printf("\n[S] All frames received successfully!\n");
+    close(sock);
     return 0;
 }
