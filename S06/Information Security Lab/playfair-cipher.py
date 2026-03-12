6c1f492f10f1dc1817fd1a95f392b27ab5cd9e83
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2026-03-12 20:35:57 +0530

    Update playfair-cipher.py

--- S06/Information Security Lab/playfair-cipher.py
+++ S06/Information Security Lab/playfair-cipher.py
@@ -1,45 +1,51 @@
-def matrix(key):
-    key = key.replace("J", "I")
-    letters = []
-    for c in key + "ABCDEFGHIKLMNOPQRSTUVWXYZ":
-        if c not in letters and c.isalpha():
-            letters.append(c)
-    return [letters[i:i+5] for i in range(0, 25, 5)]
+import numpy as np
 
-def pairs(text):
-    text = text.replace("J", "I")
-    i, res = 0, ""
-    while i < len(text):
-        a = text[i]
-        b = text[i+1] if i+1 < len(text) else "X"
-        if a == b:
-            res += a + "X"
-            i += 1
-        else:
-            res += a + b
-            i += 2
-    return res + ("X" if len(res) % 2 else "")
+key = "MONARCHY".replace('J', 'I')
+domain = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".replace('J', 'I')
 
-def pos(m, c):
-    return next((i, j) for i in range(5) for j in range(5) if m[i][j] == c)
+k = ""
 
-def cipher(text, m, step):
-    return "".join(
-        m[r][(c+step)%5] + m[r2][(c2+step)%5] if r == r2 else
-        m[(r+step)%5][c] + m[(r2+step)%5][c2] if c == c2 else
-        m[r][c2] + m[r2][c]
-        for r, c, r2, c2 in
-        ( (*pos(m, text[i]), *pos(m, text[i+1])) for i in range(0, len(text), 2) )
-    )
+for ch in key + domain:
+    if ch not in k:
+        k += ch
 
-text = input("Enter Text: ").upper().replace(" ", "")
-key = "MONARCHY"
+matrix = np.array(list(k)).reshape(5, 5)
 
-m = matrix(key)
-prepared = pairs(text)
+text = input("Plain Text: ").upper().replace('J', 'I').replace(" ", "")
 
-encrypted = cipher(prepared, m, 1)
-decrypted = cipher(encrypted, m, -1)
+if len(text)%2 != 0:
+    text += 'X'
 
-print("\nEncrypted Text:", encrypted)
-print("Decrypted Text:", decrypted)
+pairs = []
+i = 0
+
+while i < len(text):
+    a = text[i]
+    b = text[i+1] if i+1 < len(text) else 'X'
+
+    if a == b:
+        pairs.append(a + 'X')
+        i += 1
+    else:
+        pairs.append(a + b)
+        i += 2
+
+cipher = ""
+
+for p in pairs:
+    a, b = p
+
+    r1, c1 = np.where(matrix==a)
+    r2, c2 = np.where(matrix==b)
+
+    r1, c1 = r1[0], c1[0]
+    r2, c2 = r2[0], c2[0]
+
+    if r1==r2:
+        cipher += matrix[r1, (c1+1)%5] + matrix[r2, (c2+1)%5]
+    elif c1==c2:
+        cipher += matrix[(r1+1)%5, c1] + matrix[(r2+1)%5, c2]
+    else:
+        cipher += matrix[r1, c2] + matrix[r2, c1]
+
+print(cipher)
