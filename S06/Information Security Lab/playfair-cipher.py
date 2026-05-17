5db16ddca2c082553b66f8863ef23b07c446e123
ABHAY <133576797+ABHAY-100@users.noreply.github.com>
2026-05-17 08:41:09 +0530

    Implement decryption for Playfair cipher

--- S06/Information Security Lab/playfair-cipher.py
+++ S06/Information Security Lab/playfair-cipher.py
@@ -18,7 +18,6 @@ i = 0
 
 while i < len(text):
     a = text[i]
-    b = text[i+1]
 
     if i + 1 < len(text):
         b = text[i+1]
@@ -51,3 +50,28 @@ for p in pairs:
         cipher += matrix[r1, c2] + matrix[r2, c1]
 
 print(cipher)
+
+pairs.clear()
+
+plain = ""
+
+for i in range(0, len(cipher), 2):
+    pairs.append(cipher[i:i+2])
+
+for p in pairs:
+    a, b = p
+
+    r1, c1 = np.where(matrix==a)
+    r2, c2 = np.where(matrix==b)
+
+    r1, c1 = r1[0], c1[0]
+    r2, c2 = r2[0], c2[0]
+
+    if r1==r2:
+        plain += matrix[r1, (c1-1)%5] + matrix[r2, (c2-1)%5]
+    elif c1==c2:
+        plain += matrix[(r1-1)%5, c1] + matrix[(r2-1)%5, c2]
+    else:
+        plain += matrix[r1, c2] + matrix[r2, c1]
+
+print(plain)
